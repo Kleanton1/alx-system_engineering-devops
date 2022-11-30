7ad40aa7342e2d3eddbec24b97955d8a3d0434a2
Kleanton1 <ifeanyiemmanuel271@gmail.com>
2022-11-30 01:58:34 +0000

    Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+Queries and prints the titles of top 10 hot posts for a given subreddit
+'''
+from requests import get
+
+
+def top_ten(subreddit):
+	'''
+	Description:
+		- queries Reddit API to print first 10 host posts
+	Output:
+		- Success: prints first 10 host posts listed
+		for a given subreddit
+		- Failure: print None
+	'''
+	endpoint = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+	headers = {'User-Agent': 'My User Agent 1.0'}
+	params = {'limit': 10}
+	res = get(endpoint, headers=headers, allow_redirects=False, params=params)
+
+	if res.status_code != 200:
+		print(None)
+		return
+
+	top_10 = res.json()['data']['children']
+	[print(Post['data']['title']) for post in top_10]
